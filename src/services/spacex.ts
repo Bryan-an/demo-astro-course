f3ffcb614c692444e1c5726fa3c5770c9deb8f7f
Bryan-an <bryanandagoya@gmail.com>
2023-09-13 21:18:58 -0500

    remove console logs

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -3,7 +3,6 @@ import { type APISpaceXResponse, type Doc } from '../types/api';
 export const getLaunchById = async (id: string) => {
   const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
   const launch = (await res.json()) as Doc;
-  console.log(launch);
   return launch;
 };
 
